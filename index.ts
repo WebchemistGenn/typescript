3ab5c7c32a95c9bcf229f684ea82f4e1dde7fb3c
yoonwonyoul <yoonwonyoul@webchemist.net>
2019-02-23 16:11:35 +0900

    feat: first commit

---
+++ index.ts
@@ -0,0 +1,12 @@
+// tslint:disable no-console
+
+// String
+const title: string = "Welcome Webchemist";
+// Number
+const count: number = 10;
+// Array
+const users: IUser[] = [{ name: "Lee", age: 20 }, { name: "Park", age: 32 }];
+
+console.log(title);
+console.log(count);
+console.log(users);
